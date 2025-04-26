f3851a3b9d67047360b8a7ee4f766ee73f0848cb
Matt Caldwell <StoatPower@users.noreply.github.com>
2025-04-26 17:33:26 -0400

    made adjustments and fixes to logic, added random session id for unique names of photos

--- retropix/retropix.ino
+++ retropix/retropix.ino
@@ -47,13 +47,17 @@
 // Buffer size for storing chunks of image data before writing to SD 
 #define IMG_BUFFER_SIZE 0xFF    // 0xFF = 255 bytes
 
+// Session ID, used in generating photo IDs since we don't
+// have a good way to get the date/time w/out extra components
+long session_id;
+
 // Global debug log level threshold
-uint8_t global_debug = DEBUG_ERR;
+uint8_t global_debug = DEBUG_INFO;
 
 // SD Card declarations
 const int SD_CS = 10;       // chip select pin for SD card
 uint8_t img_count = 0;      // image file counter (i.e., 0.jpg, 1.jpg, ...)
-char img_name[10] = {0};    // buffer to hold generated filenames
+char img_name[20] = {0};    // buffer to hold generated filenames
 File out_file;              // File object for the SD card
 uint8_t current_img_byte = 0;
 uint8_t next_img_byte = 0;
@@ -68,6 +72,7 @@ Arducam_Mega mega(MEGA_CS); // initialize camera using chip select pin
 // Photodiode Declarations
 char pd_message[50];                // debug message for photodiode readings
 bool is_taking_picture = false;     // flag for determining when a picture is being taken (or not)
+bool picture_taken = false;
 bool picture_saved = false;
 const int pd_threshold = 2;         // must have n consecutive readings of ambient light detection to trigger a photo
 uint8_t pd_threshold_counter = 0;   // counter for threshold
@@ -83,11 +88,19 @@ int readAndUpdateAmbientLight();
 void takeAndSavePicture();
 void awaitShutterClose();
 
-void setup() {
+void setup() {   
+  Serial.begin(IMG_BAUD); // Set Serial Monitor  BAUD Rate
+
   pinMode(SD_CS, OUTPUT); // Set the SD Card chip select pin to output mode
 
-  //// Arducam Mega setup
-  Serial.begin(IMG_BAUD);
+  // Seed randomness for session ID (get some entropy from unused analog pin 1)
+  randomSeed(analogRead(A1));
+  session_id = random(100000,999999); // 6-digit session ID
+  char id_msg[20];
+  sprintf(id_msg, "Session ID: %ld", session_id);
+  log(id_msg, DEBUG_INFO);
+
+  // Arducam Mega setup
   log(F("Hello Arduino UNO!"), DEBUG_INFO);
   // Initialize camera configuration, defaulting to JPEG data format at maximum resolution
   mega.begin();
@@ -101,12 +114,37 @@ void setup() {
   log(F("SD Card detected."), DEBUG_INFO);
 }
 
+ 
+/** loop/0
+ * 
+ * Main Loop
+ * While not currently taking and processing a picture,
+ * wait for the shutter to open and light to be detected.
+ * 
+ * When detected light meets our threshold, switch to
+ * picture taking and saving mode until save is successful
+ * (or an error occurs).
+ *
+ * When picture has been saved, wait for the shutter to close
+ * and then switch back to standby mode waiting for next picture.
+ *
+ * Runs until powered off.
+ *
+ * return `void`
+ */
 void loop() {
-  // if we aren't currently taking a picture...
+  // int pd_value = analogRead(A0);
+  // sprintf(pd_message, "Photodiode Reading: %d", pd_value);
+  // log(pd_message, DEBUG_INFO);
+  // delay(500);
+
   if (!is_taking_picture) {
     handleAmbientLightDetection();
-  } else if (!picture_saved) {
-      takeAndSavePicture();
+  } else if (is_taking_picture && !picture_saved) {
+    takeAndSavePicture();    
+    // log(F("faking picture save"), DEBUG_INFO);
+    // delay(1000);
+    // picture_saved = true;
   } else {
     awaitShutterClose();
   }  
@@ -134,53 +172,59 @@ void handleAmbientLightDetection() {
   }
 }
 
-void takeAndSavePicture() {  
-  log(F("Smile! You're on candid camera!"), DEBUG_INFO);
+void takeAndSavePicture() {
+  if (!picture_taken) {
+    log(F("Smile! You're on candid camera!"), DEBUG_INFO);
+    // CAM_IMAGE_MODE_QVGA is 320x240
+    mega.takePicture(CAM_IMAGE_MODE_QVGA, CAM_IMAGE_PIX_FMT_JPG);
+    picture_taken = true;
+  } else {
+    // while there is still data to receive and while picture hasn't been saved
+    // continue to process data
+    while (mega.getReceivedLength() && !picture_saved) {
+      current_img_byte = next_img_byte;
+      next_img_byte = mega.readByte();
 
-  // CAM_IMAGE_MODE_QVGA is 320x240
-  mega.takePicture(CAM_IMAGE_MODE_QVGA, CAM_IMAGE_PIX_FMT_JPG);
+      // save next byte of data to file
+      if (jpg_head_flag == 1) {
+        img_buffer[img_buffer_idx++] = next_img_byte;
+        if (img_buffer_idx >= IMG_BUFFER_SIZE) {
+          out_file.write(img_buffer, img_buffer_idx);
+          img_buffer_idx = 0;
+        }
+      }
 
-  // while there is still data to receive and while picture hasn't been saved
-  // continue to process data
-  while (mega.getReceivedLength() && !picture_saved) {
-    current_img_byte = next_img_byte;
-    next_img_byte = mega.readByte();
+      // start of image marker detected
+      // name and open new file and begin saving it
+      if (current_img_byte == SOI_1 && next_img_byte == SOI_2) {
+        jpg_head_flag = 1;
+        sprintf(img_name, "%d-%d.jpg", session_id, img_count);
+        img_count++;
+        out_file = SD.open(img_name, FILE_WRITE | FILE_READ);
+        if (!out_file) {
+          log(F("File open failed. Resetting camera. Please ensure SD card is inserted."), DEBUG_ERR);
+          picture_taken = false;
+          is_taking_picture = false;
+          mega.reset();
+          break;
+        }
+        img_buffer[img_buffer_idx++] = current_img_byte;
+        img_buffer[img_buffer_idx++] = next_img_byte;
+      }
 
-    // save next byte of data to file
-    if (jpg_head_flag == 1) {
-      img_buffer[img_buffer_idx++] = next_img_byte;
-      if (img_buffer_idx >= IMG_BUFFER_SIZE) {
+      // end of image marker detected
+      // write out last bytes of file and clean up file handler
+      // mark picture as saved, break out of loop
+      if (current_img_byte == EOI_1 && next_img_byte == EOI_2) {
+        jpg_head_flag = 0;
         out_file.write(img_buffer, img_buffer_idx);
         img_buffer_idx = 0;
-      }
-    }
-
-    // start of image marker detected
-    // name and open new file and begin saving it
-    if (current_img_byte == SOI_1 && next_img_byte == SOI_2) {
-      jpg_head_flag = 1;
-      sprintf(img_name, "%d.jpg", img_count);
-      img_count++;
-      out_file = SD.open(img_name, FILE_WRITE | FILE_READ);
-      if (!out_file) {
-        log(F("File open failed."), DEBUG_ERR);
+        out_file.close();
+        picture_saved = true;
+        picture_taken = false;
+        log(F("Image save succeeded."), DEBUG_INFO);
         break;
       }
-      img_buffer[img_buffer_idx++] = current_img_byte;
-      img_buffer[img_buffer_idx++] = next_img_byte;
-    }
-
-    // end of image marker detected
-    // write out last bytes of file and clean up file handler
-    // mark picture as saved, break out of loop
-    if (current_img_byte == EOI_1 && next_img_byte == EOI_2) {
-      jpg_head_flag = 0;
-      out_file.write(img_buffer, img_buffer_idx);
-      img_buffer_idx = 0;
-      out_file.close();
-      picture_saved = true;
-      log(F("Image save succeeded."), DEBUG_INFO);
-      break;
     }
   }
 }
@@ -216,12 +260,15 @@ void log(const __FlashStringHelper* message, int debug_lvl) {
   switch (debug_lvl) {
     case DEBUG_INFO:
       Serial.println(message);
+      break;
 
     case DEBUG_WARN:
       Serial.println(message);
+      break;
 
     case DEBUG_ERR:
       Serial.println(message);
+      break;
 
     default:
       break;
@@ -235,12 +282,15 @@ void log(const char* message, int debug_lvl) {
   switch (debug_lvl) {
     case DEBUG_INFO:
       Serial.println(message);
+      break;
 
     case DEBUG_WARN:
       Serial.println(message);
+      break;
 
     case DEBUG_ERR:
       Serial.println(message);
+      break;
 
     default:
       break;
